05ca0d67589e850b4d4d36e4198559a49f6d5412
DK\obr <obr@sitecore.net>
2018-02-28 12:52:55 +0200

    Init

---
+++ src/Sitecore.Support.209312/ContentSearch/Azure/CloudSearchDocumentBuilder.cs
@@ -0,0 +1,35 @@
+﻿namespace Sitecore.Support.ContentSearch.Azure
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Concurrent;
+    using Sitecore.ContentSearch;
+    using Sitecore.ContentSearch.Azure.Config;
+    using Sitecore.ContentSearch.Azure.Schema;
+    using Sitecore.ContentSearch.Azure.Utils;
+    using Sitecore.ContentSearch.ComputedFields;
+    using Sitecore.ContentSearch.Diagnostics;
+    using System.Globalization;
+
+    public class CloudSearchDocumentBuilder : Sitecore.ContentSearch.Azure.CloudSearchDocumentBuilder
+    {
+        public CloudSearchDocumentBuilder(IIndexable indexable, IProviderUpdateContext context) : base(indexable, context)
+        {
+        }
+
+        protected override void AddComputedIndexField(IComputedIndexField computedIndexField, object fieldValue)
+        {
+            if (fieldValue is IEnumerable && fieldValue.GetType().IsGenericType)
+            {
+                foreach (var field in fieldValue as IEnumerable)
+                {
+                    this.AddField(computedIndexField.FieldName, field);
+                }
+            }
+            else
+            {
+                this.AddField(computedIndexField.FieldName, fieldValue);
+            }
+        }
+    }
+}
---
+++ src/Sitecore.Support.209312/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.209312")]
+[assembly: AssemblyProduct("Sitecore.Support.209312")]
+[assembly: ComVisible(false)]
